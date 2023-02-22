cdf6ae3d947621caa708a26fcc0339084219e0bd
flpdorea <felipeldorea@gmail.com>
2023-02-22 15:35:08 -0300

    add transactions tests

---
+++ test/transactions.test.ts
@@ -0,0 +1,48 @@
+import { afterAll, beforeAll, test, describe, expect } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'title',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  test('list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'title',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'title',
+        amount: 5000,
+      }),
+    ])
+  })
+})
